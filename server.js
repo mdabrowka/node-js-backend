a25e9ea67cad67fabec9e59a3615fb454bb00698
Marta <martadabrowka@gmail.com>
2018-08-11 20:03:51 +0100

    installs express, creates basic html

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+// app.get('/', function (req, res) {
+//   res.json({ data: 'Suppp!' })
+// });
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+})
+
+
+app.listen(3000, function () {
+  console.log('App running on port ' + this.address().port);
+});
