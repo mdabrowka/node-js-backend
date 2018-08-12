84e4a89328c81bdc110ff99d97e83874911dcd40
Marta <martadabrowka@gmail.com>
2018-08-12 19:14:58 +0100

    adds getQuestions request

--- server.js
+++ server.js
@@ -3,9 +3,17 @@ var app = express();
 
 app.use(express.static('public'));
 
-app.get('/', function(req, res) {
+server.use(express.static('public'));
+
+server.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
-})
+});
+
+const questions = [{question_1: 'Do you like dogs?'}, {question_2: 'Do you speak Mandarin?'} ]
+
+server.get('/api/get-questions', (req, res) => {
+  res.send(questions);
+});
 
 
 app.listen(3000, function () {
